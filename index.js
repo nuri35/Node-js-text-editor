1d97d7509a22b48beeec7b1b74dc67f313830cf9
Nurettin Şen <nurie487@gmail.com>
2021-10-28 10:24:02 +0300

    link copy image

--- index.js
+++ index.js
@@ -4,10 +4,49 @@ import Header from '@editorjs/editorjs';
 
 import List from '@editorjs/editorjs';
 import Embed from '@editorjs/editorjs';
+const Checklist = require('@editorjs/checklist');
+const SimpleImage = require('@editorjs/simple-image');
+
+
 
 const editor = new EditorJS({
  holderId:"editorjs",
  tools:{
-   class:Header
+   header:{
+    class:Header,
+    inlineToolbar:["link"]
+   },
+   list: {
+    class: List,
+    inlineToolbar:["link","bold"],
+  },
+  embed: {
+    class: Embed,
+    config: {
+      services: {
+        youtube: true,
+        coub: true
+      }
+    }
+  },
+ 
+  image: SimpleImage,
+  checklist: {
+    class: Checklist,
+    inlineToolbar: true,
+  },
+
+  
  }
 });
+
+
+let savebtn = document.querySelector(".button")
+
+savebtn.addEventListener("click",()=>{
+  editor.saver().then((result) => {
+    console.log("article data" + result)
+  }).catch((err) => {
+    console.log("failed:" + err)
+  });
+})
