9e782d935b30568b348c0b474e5f17e13c535a54
Nurettin Şen <nurie487@gmail.com>
2021-10-28 09:44:08 +0300

    inital value

---
+++ index.js
@@ -0,0 +1,11 @@
+import EditorJS from '@editorjs/editorjs';
+
+import Header from '@editorjs/editorjs';
+
+import List from '@editorjs/editorjs';
+import Embed from '@editorjs/editorjs';
+
+const editor = new EditorJS({
+ holderId:"editorjs",
+ 
+});
